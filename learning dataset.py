23a5be3bace564a96bb2f5bc0960ce4780b36822
bhatt-kanishka <kanishkabhatt93@gmail.com>
2026-06-11 22:20:07 +0530

    Updated learning dataset

--- learning dataset.py
+++ learning dataset.py
@@ -1,5 +1,17 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score
+)
+
 
 df = pd.read_csv(r"C:\Users\HP\Downloads\train.csv")
 
@@ -41,25 +53,57 @@ df["Embarked"] = df["Embarked"].map({
     })
 
 print(df["Embarked"].head())
+print(df.columns)
+
 
 y = df["Survived"]
 
 X = df[
-    [
-        "Pclass",
-        "Sex",
-        "Age",
-        "SibSp",
-        "Parch",
-        "Fare",
-        "Embarked"
+        [
+            "Pclass",
+            "Sex",
+            "Age",
+            "Parch",
+            "Fare",
+            "Embarked"
     ]
 ]
 
+
 X_train , X_test , y_train , y_test = train_test_split(X , y , test_size=0.2 , random_state=42)
 
-print(X_train.shape)
 
-print(X_test.shape)
-print(X.columns)
-print(X.shape)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train , y_train)
+y_pred = model.predict(X_test)
+
+
+cm = confusion_matrix(y_test , y_pred)
+print(cm)
+
+knn = KNeighborsClassifier(n_neighbors=9)
+
+knn.fit(X_train_scaled, y_train)
+
+y_pred_knn = knn.predict(X_test_scaled)
+
+print("KNN Accuracy :", accuracy_score(y_test, y_pred_knn))
+print("KNN Precision :", precision_score(y_test, y_pred_knn))
+print("KNN Recall :", recall_score(y_test, y_pred_knn))
+print("KNN F1 Score :", f1_score(y_test, y_pred_knn))
+
+nb = GaussianNB()
+nb.fit(X_train , y_train)
+y_pred_nb = nb.predict(X_test)
+
+print("\nNB Accuraccy : ",accuracy_score(y_test , y_pred_nb))
+print("NB presecion : ",precision_score(y_test , y_pred_nb))
+print("NB recall : ",recall_score(y_test , y_pred_nb))
+print("NB F1 score : ",f1_score(y_test , y_pred_nb))
